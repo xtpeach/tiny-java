196dfe26a27d1b70e6919f0e1789022b5abfdeae
xtpeach <xtpeach@outlook.com>
2024-12-26 00:41:43 +0800

    最小java-springboot项目

--- src/main/java/com/xtpeach/minimal/controller/HelloController.java
+++ src/main/java/com/xtpeach/minimal/controller/HelloController.java
@@ -7,8 +7,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-    @GetMapping("/hello")
+    // http://patorjk.com/software/taag/#p=display&h=0&v=1&f=ANSI%20Shadow&t=minima
+    @GetMapping(value = "/hello")
     public String hello() {
-        return "Hello, World!";
+        return "minimal";
     }
 }
